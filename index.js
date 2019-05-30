52634bb3b838987239de4f9a3e79f57d8f286f04
amusing-history-4188 <mackenzieerinpech@gmail.com>
2019-05-30 00:37:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
-function shout(String) {
-  return String.toUpperCase();
+function shout(string){
+  return string.toUpperCase()
 }
-function whisper(String) {
-  return String.toLowerCase();
-}
-function logShout(String) {
-  console.log(String.toUpperCase());
-}
-function logWhisper(String) {
-  console.log(String.toLowerCase());
-}
-function sayHiToGrandma(string){
-  var uppercase = "HELLO"
-  var lowercase = "hello"
 
-  if (string === lowercase){
+function whisper(string){
+  return string.toLowerCase()
+}
+
+
+function logShout(string){
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string){
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string){
+
   if (string === string.toLowerCase()){
     return "I can't hear you!"
-  }else if (string === uppercase){
   }else if (string === string.toUpperCase()){
     return "YES INDEED!"
   }else if (string === "I love you, Grandma."){
     return "I love you, too."
+  }
+}
