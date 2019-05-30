a3d42d218957444f4d62003e5e42ce6e95e3db48
amusing-history-4188 <mackenzieerinpech@gmail.com>
2019-05-30 00:38:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ function whisper(string){
   return string.toLowerCase()
 }
 
-
 function logShout(string){
   console.log(string.toUpperCase())
 }
@@ -19,9 +18,11 @@ function sayHiToGrandma(string){
 
   if (string === string.toLowerCase()){
     return "I can't hear you!"
-  }else if (string === string.toUpperCase()){
+  }
+  else if (string === string.toUpperCase()){
     return "YES INDEED!"
-  }else if (string === "I love you, Grandma."){
+  }
+  else if (string === "I love you, Grandma."){
     return "I love you, too."
   }
 }
