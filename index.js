290bd7589e99f7b4e17125cb6df8b369c1945c01
amusing-history-4188 <mackenzieerinpech@gmail.com>
2019-05-30 00:34:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 function shout(String) {
   return String.toUpperCase();
-}
+
 function whisper(String) {
   return String.toLowerCase();
-}
+
 function logShout(String) {
   console.log(String.toUpperCase());
-}
+
 function logWhisper(String) {
   console.log(String.toLowerCase());
+
+function sayHiToGrandma(String) {
+
+}
+}
+}
+}
 }
